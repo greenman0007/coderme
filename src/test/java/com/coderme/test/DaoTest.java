28da56e553d027539cc834bd256ca1db8d86d0d2
greenman0007 <greenman0007@gmail.com>
2014-07-01 10:41:19 +0800

    commit first

---
+++ src/test/java/com/coderme/test/DaoTest.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)DaoTest.java 2013-12-11
+ * 
+ * Copy Right@ coderme.cn
+ */ 
+
+package com.coderme.test;
+
+
+/**
+ * <pre>
+ * @author coderme
+ *
+ *
+ * 创建日期: 2013-12-11
+ * 修改人 :  
+ * 修改说明: 
+ * 评审人 ：
+ * </pre>
+ */
+public class DaoTest {
+
+}
